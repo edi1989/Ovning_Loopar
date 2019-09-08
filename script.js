bcf4c180cc9eadaf1784a28653eee7a699452070
Edi Sogorovic <soed19bz@student.ju.segit config --global user.email soed19bz@student.ju.se>
2019-09-08 20:13:26 +0200

    Lagt till kommentarer

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var body = document.querySelector("body");
 
 var div = document.createElement("div")
+
+//I din Js-fil skapa en array med minst 5 st saker du tycker är kul.
 var array = ['Badminton','Padel','Fotboll','Pingis','Simma']
 arraylangd = array.length;
+
 var listaUL = document.createElement("ul");
 
+//Skriv en for loop som skriver ut din array till html-sidan som en lista <ul> med varje element i din array som en <li>
 for (let i = 0; i < arraylangd; i++) {
     var listaLI = document.createElement("li");
     listaLI.innerHTML = array[i];
